fcacca475db3c8dbafc5a1f3f8bbe7dc8d0883e6
galgadet <51114061+galgadet@users.noreply.github.com>
2019-05-28 14:17:33 +0530

    Create Geeksforgeeks .java

---
+++ Geeksforgeeks .java
@@ -0,0 +1,10 @@
+public class Geeksforgeeks 
+{ 
+    public static void main(String[] args) 
+    { 
+        System.out.println(2+0+1+6+"GeeksforGeeks"); 
+        System.out.println("GeeksforGeeks"+2+0+1+6); 
+        System.out.println(2+0+1+5+"GeeksforGeeks"+2+0+1+6); 
+        System.out.println(2+0+1+5+"GeeksforGeeks"+(2+0+1+6)); 
+    } 
+} 
